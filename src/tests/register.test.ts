6c26a72a272e1f5d7ff26d5597d22dd4c7cec794
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-03-21 18:09:33 +0900

    setting up test database with typeorm

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,8 +1,12 @@
 import { request } from "graphql-request";
-import { createConnection } from "typeorm";
+import { createTypeormConn } from "../utils/createTypeormConn";
 
 import { host } from "./constants";
 import { User } from "../entity/User";
+
+beforeAll(async () => {
+  await createTypeormConn();
+});
 
 const email = "tom@bob.com";
 const password = "bob";
@@ -16,7 +20,6 @@ mutation {
 test("Register User", async () => {
   const response = await request(host, mutation);
   expect(response).toEqual({ register: true });
-  await createConnection();
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
